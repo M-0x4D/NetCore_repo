001dd077c2214b056b4df957942d145b512e20a2
mohamed adel <56012558+programmermohamedadel@users.noreply.github.com>
2021-12-30 07:39:23 +0200

    Add files via upload

---
+++ new_models/TblDatum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace demo_api.new_models
+{
+    public partial class TblDatum
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long? Number { get; set; }
+    }
+}
---
+++ new_models/newdatabaseContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace demo_api.new_models
+{
+    public partial class newdatabaseContext : DbContext
+    {
+        public newdatabaseContext()
+        {
+        }
+
+        public newdatabaseContext(DbContextOptions<newdatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblDatum> TblData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-26P7ENM\\MOHAMED;Database=newdatabase;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Arabic_CI_AS");
+
+            modelBuilder.Entity<TblDatum>(entity =>
+            {
+                entity.ToTable("tbl_data");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Number).HasColumnName("number");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
